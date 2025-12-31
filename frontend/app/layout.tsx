454cdd18d6fea5c7811162785f7e8d29bce1b1be
Boris D <b88008800@gmail.com>
2025-12-31 14:44:39 +0000

    Bundle Material Symbols for offline use

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,13 +21,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en-GB">
-      <head>
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
-        />
-      </head>
       <body
         className={`${inter.variable} ${sourceSerif.variable} bg-background-light dark:bg-background-dark text-slate-900 dark:text-white min-h-screen`}
       >
--- transfer-line-travel/index.tsx
+++ transfer-line-travel/index.tsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@fontsource/material-symbols-outlined/400.css';
+import './material-symbols.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
